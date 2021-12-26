749aec1ec2a51cd6b7a3ab3cd3a3aa45f082c267
Yunchuan Chen <expnn2ai@gmail.com>
2021-12-26 20:39:23 +0800

    feat: 统一 macOs 与 Windows 平台下使用的rust自定义编译目标配置文件。通过在 local.properties 文件中添加 hwndk.dir 配置项， 指向NDK根目录，并在构建脚本中使用该配置项实现。

--- entry/src/main/rust/build.bat
+++ entry/src/main/rust/build.bat
@@ -6,17 +6,19 @@ for %%x in (%*) do (
     set /A argC+=1
 )
 
-if %argC% NEQ 2 (
+if %argC% NEQ 3 (
     echo Illegal number of parameters
-    echo Usage: $0 build_type arch_abi
+    echo Usage: $0 ndk_dir build_type arch_abi
     exit /b 1
 )
 
-set build_type=%1
-set arch_abi=%2
+set ndk_dir=%1
+set build_type=%2
+set arch_abi=%3
 
-echo %build_type%
-echo %arch_abi%
+echo [RustLibBuilder] ndk_dir = %ndk_dir%
+echo [RustLibBuilder] build_type = %build_type%
+echo [RustLibBuilder] arch_abi = %arch_abi%
 
 set "working_dir=%cd%"
 set "build_dir=%working_dir%/.rust/%build_type%/%arch_abi%"
@@ -39,20 +41,34 @@ if not exist "%output_dir%" (
 pushd .
 cd "%rust_source_root%"
 
+set "gcc_toolchain=%ndk_dir%/llvm"
+set "sysroot=%ndk_dir%/sysroot"
+set "linker=%gcc_toolchain%/bin/clang.exe"
+echo [RustLibBuilder] gcc_toolchain = %gcc_toolchain%
+echo [RustLibBuilder] sysroot = %sysroot%
+echo [RustLibBuilder] linker = %linker%
+
+set "CARGO_BUILD_OLD_RUSTFLAGS=%CARGO_BUILD_RUSTFLAGS%"
+set "CARGO_BUILD_RUSTFLAGS=-C linker=%linker% -C link-args=--gcc-toolchain=%gcc_toolchain% --sysroot=%sysroot%"
+
+echo [RustLibBuilder] CARGO_BUILD_RUSTFLAGS = %CARGO_BUILD_RUSTFLAGS%
+
 if "%build_type%" == "Release" (
     cargo +nightly build ^
       -Z unstable-options --release --lib ^
       --target-dir "%build_dir%" ^
       --out-dir "%output_dir%"   ^
       -Zbuild-std=panic_abort,std ^
-      --target aarch64-linux-ohos-win.json
+      --target aarch64-linux-ohos.json
 ) else (
     cargo +nightly build ^
       -Z unstable-options --lib ^
       --target-dir "%build_dir%" ^
       --out-dir "%output_dir%"   ^
       -Zbuild-std=panic_abort,std ^
-      --target aarch64-linux-ohos-win.json
+      --target aarch64-linux-ohos.json
 )
+
+set "CARGO_BUILD_RUSTFLAGS=%CARGO_BUILD_OLD_RUSTFLAGS%"
 
 popd
--- entry/src/main/rust/build.sh
+++ entry/src/main/rust/build.sh
@@ -3,17 +3,22 @@
 set -e
 set -o pipefail
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Illegal number of parameters"
-    echo "Usage: $0 <build_type> <arch_abi>"
+    echo "Usage: $0 <ndk_dir> <build_type> <arch_abi>"
     exit 1
 fi
 
-build_type=$1
-arch_abi=$2
+ndk_dir=$1
+build_type=$2
+arch_abi=$3
+
+echo "[RustLibBuilder] ndk_dir = ${ndk_dir}"
+echo "[RustLibBuilder] build_type = ${build_type}"
+echo "[RustLibBuilder] arch_abi = ${arch_abi}"
 
 working_dir=$PWD
-build_dir="${working_dir}"/.rust/"${build_type}/${arch_abi}"
+build_dir="${working_dir}/.rust/${build_type}/${arch_abi}"
 output_dir="${working_dir}"/libs/"${arch_abi}"
 rust_source_root="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
@@ -28,7 +33,19 @@ mkdir -p "$output_dir"
 
 cd "${rust_source_root}"
 
+gcc_toolchain="${ndk_dir}/llvm"
+sysroot="${ndk_dir}/sysroot"
+linker="${gcc_toolchain}/bin/clang"
+
+echo "[RustLibBuilder] gcc_toolchain = $gcc_toolchain"
+echo "[RustLibBuilder] sysroot = $sysroot"
+echo "[RustLibBuilder] linker = $linker"
+
+rustflags="-C linker=\"${linker}\" -C link-args=--gcc-toolchain=\"${gcc_toolchain}\" --sysroot=\"${sysroot}\""
+echo "[RustLibBuilder] CARGO_BUILD_RUSTFLAGS = ${rustflags}"
+
 if [[ "${build_type}" = "Release" ]]; then
+    CARGO_BUILD_RUSTFLAGS="${rustflags}" \
     cargo +nightly build \
       -Z unstable-options --release --lib \
       --target-dir "$build_dir" \
@@ -36,6 +53,7 @@ if [[ "${build_type}" = "Release" ]]; then
       -Zbuild-std=panic_abort,std \
       --target aarch64-linux-ohos.json
 else
+    CARGO_BUILD_RUSTFLAGS="${rustflags}" \
     cargo +nightly build \
       -Z unstable-options --lib \
       --target-dir "$build_dir" \
@@ -43,4 +61,3 @@ else
       -Zbuild-std=panic_abort,std \
       --target aarch64-linux-ohos.json
 fi
-
