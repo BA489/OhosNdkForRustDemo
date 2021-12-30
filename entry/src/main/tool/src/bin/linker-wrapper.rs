f7bbea845f3f1ba51bf9afc791ac629ee285b4a6
Yunchuan Chen <expnn2ai@gmail.com>
2021-12-30 21:31:30 +0800

    fix: 解决Windows上命令行超长后使用response file导致参数不能正确被删除的问题

--- entry/src/main/tool/src/bin/linker-wrapper.rs
+++ entry/src/main/tool/src/bin/linker-wrapper.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::process::Command;
 use anyhow::{Context, Result, bail};
 
@@ -6,12 +7,7 @@ fn main() -> Result<()> {
     let c_linker = env::var("C_LINKER")
         .with_context(|| format!("Failed to get C_LINKER environment variable. "))?;
 
-    let mut args = vec![];
-    for arg in env::args().skip(1) {
-        if arg != "-llog" {
-            args.push(arg);
-        }
-    }
+    let args = collect_cleaned_args()?;
 
     let mut cmd = Command::new(&c_linker);
     cmd.args(args);
@@ -28,3 +24,28 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+fn collect_cleaned_args() -> Result<Vec<String>> {
+    let os = env::consts::OS;
+    if os != "windows" && os != "macos" {
+        bail!("Unsupported operation system: {}", os);
+    }
+
+    let mut args = vec![];
+    for arg in env::args().skip(1) {
+        if arg != "-llog" {
+            args.push(arg);
+        }
+    }
+
+    // see https://stackoverflow.com/questions/40727748/windows-clang-command-line-too-long
+    if os == "windows" && args.len() == 1 && args[0].starts_with("@") {
+        let filename = &args[0][1..];
+        // 用下面一行来确定 response file 的格式是每行一个参数。直接将-llog行删除掉即可。
+        // fs::write("liker-args.txt", &contents)?;
+        let contents = fs::read_to_string(filename)?
+            .replace("-llog\n", "");
+        fs::write(filename, &contents)?;
+    }
+
+    return Ok(args)
+}
